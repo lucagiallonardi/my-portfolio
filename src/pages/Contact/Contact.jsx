46ad8c5743f24e06d86b773481d2a05605319a5c
unknown <lgiallonardi@gmail.com>
2023-10-02 13:48:59 -0300

    error en numero de contacto

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -20,7 +20,7 @@ const location = useLocation();
             
             <Link to={'mailto:lgiallonardi@gmail.com?Subject=He%20visto%20tu%20portfolio!'} className='ContactLink' target='_blank'><div className="contactItemDiv"><i className="fa-regular fa-envelope"></i>lgiallonardi@gmail.com</div></Link>
             <Link to={'https://github.com/lucagiallonardi'} className='ContactLink' target='_blank'><div className="contactItemDiv"><i className="fa-brands fa-github"></i>https://github.com/lucagiallonardi</div></Link>
-            <Link to={'https://wa.me/1135562843'} className='ContactLink' target='_blank'><div className="contactItemDiv"><i className="fa-brands fa-whatsapp"></i>11-35562843</div></Link>
+            <Link to={'https://wa.me/+541135562843'} className='ContactLink' target='_blank'><div className="contactItemDiv"><i className="fa-brands fa-whatsapp"></i>11-35562843</div></Link>
             <Link to={'https://www.linkedin.com/in/luca-giallonardi-084992154'} className='ContactLink' target='_blank'><div className="contactItemDiv"><i class="fa-brands fa-linkedin"></i>https://www.linkedin.com/in/luca-giallonardi</div></Link>
             
             </div>
